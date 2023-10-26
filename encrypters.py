652837e50f19e2790ed51b4a0f904c1dfd9b1071
yisslu <lugohalo999@gmail.com>
2023-10-26 12:30:20 -0600

    Algoritmos chacha20 y AES-ECB listos

--- encrypters.py
+++ encrypters.py
@@ -1,8 +1,8 @@
-import abc
+import abc, os, struct
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
-import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 class Encrypter(metaclass = abc.ABCMeta):
     @abc.abstractmethod
@@ -66,5 +66,69 @@ class RSA_OAEP(Encrypter):
                 label=None
             )
         )
+    def verify(self) -> bool:
+        return self.file_content == self.decrypted_content
+    
+class Aes_ecb(Encrypter):
+    def __init__(self, file_path: str):
+        self.original_file_path = file_path 
+    
+    def prepare_encrypt(self) -> None:
+        key = os.urandom(32)
+        counter = 0
+        algorithm = algorithms.AES(key)
+        self.cipher = Cipher(algorithm, mode = modes.ECB())
+        self.encryptor = self.cipher.encryptor()
+
+        with open(self.original_file_path, 'rb') as file:
+            self.file_content = file.read()
+        
+    def encrypt(self) -> None:
+        self.encrypted_content = self.encryptor.update(self.file_content)
+
+    def post_encrypt(self) -> None:
+        result_file_path = f"{os.path.splitext(self.original_file_path)[0]}.bin"
+        with open(result_file_path, 'wb') as file:
+            file.write(self.encrypted_content)
+
+    def prepate_decrypt(self) -> None:
+        self.decryptor = self.cipher.decryptor()
+
+    def decrypt(self) -> None:
+        self.decrypted_content = self.decryptor.update(self.encrypted_content)                    
+    
+    def verify(self) -> bool:
+        return self.file_content == self.decrypted_content
+    
+class Chacha20(Encrypter):
+    def __init__(self, file_path: str):
+        self.original_file_path = file_path 
+    
+    def prepare_encrypt(self) -> None:
+        nonce = os.urandom(8)
+        key = os.urandom(32)
+        counter = 0
+        full_nonce = struct.pack("<Q",counter) + nonce
+        algorithm = algorithms.ChaCha20(key,full_nonce)
+        self.cipher = Cipher(algorithm, mode=None)
+        self.encryptor = self.cipher.encryptor()
+
+        with open(self.original_file_path, 'rb') as file:
+            self.file_content = file.read()
+        
+    def encrypt(self) -> None:
+        self.encrypted_content = self.encryptor.update(self.file_content)
+
+    def post_encrypt(self) -> None:
+        result_file_path = f"{os.path.splitext(self.original_file_path)[0]}.bin"
+        with open(result_file_path, 'wb') as file:
+            file.write(self.encrypted_content)
+
+    def prepate_decrypt(self) -> None:
+        self.decryptor = self.cipher.decryptor()
+
+    def decrypt(self) -> None:
+        self.decrypted_content = self.decryptor.update(self.encrypted_content)                    
+    
     def verify(self) -> bool:
         return self.file_content == self.decrypted_content
