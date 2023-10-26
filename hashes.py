916823df34daaa88168f8b2d0b4a2e8dd16fb606
mike71836 <mike71836@gmail.com>
2023-10-25 19:48:43 -0600

    Agregamos algoritmos sha2 y sha3

--- hashes.py
+++ hashes.py
@@ -1,5 +1,6 @@
 import abc
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.hazmat.primitives import hashes #para sha 2 y sha 3
 from os import urandom, path
 
 class Hashes(metaclass = abc.ABCMeta):
@@ -15,6 +16,58 @@ class Hashes(metaclass = abc.ABCMeta):
     @abc.abstractmethod
     def verify(self) -> bool:
         pass
+
+class sha2 (Hashes):#heredo de la clase Hashes todos sus métodos
+    def __init__(self, file_path: str):#método constructor, recibe un path y lo guarda como un atributo del objeto
+        self.original_file_path = file_path
+    
+    def prepare_hash(self) -> None:
+        self.digest = hashes.Hash(hashes.SHA512())#crea el objeto que va a crear los hashes (es un atributo del objeto sha2)
+
+        with open(self.original_file_path, 'rb') as f:
+            self.data = f.read() # Lee el contenido del archivo y lo guarda en "data"
+    
+    def hash(self) -> None:
+        self.digest.update(self.data)#Se genera el hash
+
+    def post_hash(self) -> None:
+        self.hash_result = self.digest.finalize()#Regresa el hash objenido. Regresa los bytes
+        result_file_path = f"{path.splitext(self.original_file_path)[0]}.bin"  #se guarda el resultado del hash en el archivo con extension .bin
+        with open(result_file_path, 'wb') as file:
+            file.write(self.hash_result)
+    
+    def verify(self) -> bool:
+        hash_calculado = hashes.Hash(hashes.SHA512())
+        hash_calculado.update(self.data)
+        hash_resultante = hash_calculado.finalize()
+
+        return hash_resultante == self.hash_result
+
+class sha3 (Hashes):#heredo de la clase Hashes todos sus métodos
+    def __init__(self, file_path: str):#método constructor, recibe un path y lo guarda como un atributo del objeto
+        self.original_file_path = file_path
+    
+    def prepare_hash(self) -> None:
+        self.digest = hashes.Hash(hashes.SHA3_512())#crea el objeto que va a crear los hashes (es un atributo del objeto sha3)
+
+        with open(self.original_file_path, 'rb') as f:
+            self.data = f.read() # Lee el contenido del archivo y lo guarda en "data"
+    
+    def hash(self) -> None:
+        self.digest.update(self.data)#Se genera el hash
+
+    def post_hash(self) -> None:
+        self.hash_result = self.digest.finalize()#Regresa el hash objenido. Regresa los bytes
+        result_file_path = f"{path.splitext(self.original_file_path)[0]}.bin"  #se guarda el resultado del hash en el archivo con extension .bin
+        with open(result_file_path, 'wb') as file:
+            file.write(self.hash_result)
+    
+    def verify(self) -> bool:
+        hash_calculado = hashes.Hash(hashes.SHA3_512())
+        hash_calculado.update(self.data)
+        hash_resultante = hash_calculado.finalize()
+
+        return hash_resultante == self.hash_result
 
 class ScryptAlgorithm(Hashes):
     def __init__(self, file_path: str):
