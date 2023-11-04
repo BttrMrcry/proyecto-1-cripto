7855d060531524b7327db8ed50f5f9f0ff53e4ff
Ricardo López Becerra <bettermercury@comunidad.unam.mx>
2023-11-04 15:35:06 -0600

    Actualización al readme.md

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ docker build -t crypto .
 docker run -t -d --name runner crypto
 docker exec runner python runners.py
 docker exec runner python graphics.py
-docker cp runner:/cripto-runner/results ./results
-docker cp runner:/cripto-runner/plots ./plots
+docker cp runner:/crypto-runner/results ./results
+docker cp runner:/crypto-runner/plots ./plots
 docker stop runner
 docker rm runner
 docker image rm crypto
